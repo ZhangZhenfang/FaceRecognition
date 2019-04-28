3a7bf702f9e41ef06092f641b491fe72f9e1e3c5
ZhangZhenfang <1228436617@qq.com>
2019-04-28 16:41:58 +0800

    test

--- FaceRecognitionApi/src/main/java/peer/afang/facerecognition/filter/Filter0.java
+++ FaceRecognitionApi/src/main/java/peer/afang/facerecognition/filter/Filter0.java
@@ -50,7 +50,8 @@ public class Filter0 implements Filter {
         String url = request.getRequestURL().toString();
         String uri = request.getRequestURI();
         LOGGER.info("{}, {}, {}", origin, method, uri);
-        if (origin.equals("https://www.the15373.com") || origin.equals("http://localhost:8081") || uri.contains("face/") || uri.contains("model/fakeReal") || uri.contains("model/verify") || url.contains("css")
+        if (uri.contains("face/") || uri.contains("model/fakeReal") || origin.equals("https://www.the15373.com")
+                || origin.equals("http://localhost:8081") || uri.contains("model/verify") || url.contains("css")
                 || url.contains("js") || url.contains("font") || url.contains("index") || url.contains("localhost")) {
             response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
             response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, String.valueOf(true));
